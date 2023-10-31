415e909af41a3c21e3b1c358d196825bec747a3f
dixonmyles <dixon.myles@gmail.com>
2023-10-31 00:14:37 -0400

    Can approve/unapprove reports

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './report.entity';
@@ -12,6 +12,17 @@ export class ReportsService {
   create(body: CreateReportDto, user: User) {
     const report = this.repo.create(body);
     report.user = user;
+
+    return this.repo.save(report);
+  }
+
+  async changeApproval(id: string, approved: boolean) {
+    const report = await this.repo.findOne({ where: { id: parseInt(id) } });
+    if (!report) {
+      throw new NotFoundException('report not found');
+    }
+
+    report.approved = approved;
 
     return this.repo.save(report);
   }
